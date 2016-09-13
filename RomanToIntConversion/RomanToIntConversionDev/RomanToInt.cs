96763950322b886337d0b1f11dc4c9a255851a0f
avrgithub <addanki.mahalakshmi@gmail.com>
2016-09-12 22:00:50 -0700

    TDD dev code with test failing

---
+++ RomanToIntConversion/RomanToIntConversionDev/RomanToInt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToIntConversionDev
+{
+    public class RomanToInt
+    {
+        public int Convert(string input)
+        {
+            return 0;
+        }
+
+        private static Dictionary<char, int> RomanIntMap = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+
+        public static int RomanToInteger(string roman)
+        {
+            int number = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i + 1 < roman.Length && RomanIntMap[roman[i]] < RomanIntMap[roman[i + 1]])
+                {
+                    number -= RomanIntMap[roman[i]];
+                }
+                else
+                {
+                    number += RomanIntMap[roman[i]];
+                }
+            }
+            return number;
+        }
+
+
+    }
+}
